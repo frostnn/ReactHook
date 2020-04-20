a3e136cce4774b0ee374217f715ca74a35ab5cc0
Artem <gradysnn92@yandex.ru>
2020-04-20 23:47:32 +0300

    Add Getting data from github

--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,22 @@
-import React , {useContext} from 'react'
+import React , {useContext, useState} from 'react'
 import {AlertContext} from '../context/alert/AlertContext'
+import { GithubContext } from '../context/github/GithubContext'
 
 export const Search = () => {
-  const {show} = useContext(AlertContext)
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+  const github = useContext(GithubContext)
+
   const onSubmit = event => {
-    if(event.key === 'Enter') {
-      show('This is Alert')
+    if(event.key !== 'Enter') {
+      return
+    }
+      github.clearUser()
+    if(value.trim()) {
+      alert.hide()
+      github.search(value.trim())
+    }else {
+      alert.show('Введите данные пользователя!')
     }
   }
 
@@ -14,7 +25,9 @@ export const Search = () => {
        <input 
        type="text"
        className="form-control"
+       value={value}
        placeholder="Введите ник пользователя..."
+       onChange={event => setValue(event.target.value)}
        onKeyPress={onSubmit}
        />
     </div>
